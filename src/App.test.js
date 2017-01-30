dd68b39145eaf533ca331162fd1de9debbdf7766
Olufemi Oluoje <kobomarun@gmail.com>
2017-01-30 18:11:22 +0100

    UIH-619: components snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import MainCounter from './components/Counter/MainCounter';
+import Search from './components/Search/Search';
+import SearchFixtures from './components/Search/Search.fixtures';
 
 describe('<MainCounter/>', () => {
-  it(' should render Snapshot', () => {
+  it(' should render Snapshot of MainCounter', () => {
     const component = renderer.create(
       <MainCounter />
     );
@@ -15,6 +17,24 @@ describe('<MainCounter/>', () => {
     expect(tree).toMatchSnapshot();
 
     tree.props.children;
+    tree = components.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('<Search/>', () => {
+  it(' should render Snapshot', () => {
+    const component = renderer.create(
+      <Saerch list={SearchFixtures}/>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    tree.props.handleChange;
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    tree.props.handleItemClick;
     tree = components.toJSON();
     expect(tree).toMatchSnapshot();
   });
